0c79f0e7665938f45b5f71998a9ec2a9c87475b6
Kalvin Nguyen <kalvin.win59@gmail.com>
2021-11-01 14:21:16 -0500

    added events, index, user models

---
+++ models/Event.js
@@ -0,0 +1,73 @@
+const sequelize = require('../config/connection');
+const {DataTypes, Model} = require("sequelize");
+
+
+class Event extends Model {}
+        
+    Event.init ({
+            id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+            },
+            location: {
+                type: DataTypes.STRING, 
+                allowNull: false
+            },
+
+            event_date: {
+            type: DataTypes.DATE,
+            allowNull: false
+            },
+            start_time: {
+                type: DataTypes.TIME,
+                defaultValue: '00:00:00',
+                field: 'hour',
+                allowNull: false
+                },
+            end_time: {
+                type: DataTypes.TIME,
+                defaultValue: '00:00:00',
+                field: 'hour',
+                allowNull: false
+                },
+            eventCategory: {
+                type: DataTypes.STRING, 
+                allowNull: false
+                },
+
+            user_id: {
+                type: DataTypes.INTEGER,
+                references: {
+                model: 'user',
+                key: 'id',
+                },
+                
+            },
+            },
+            {
+            sequelize,
+            timestamps: true,
+            updatedAt: false,
+            freezeTableName: true,
+            underscored: true,
+            modelName: 'thread',
+            
+            })
+
+    module.exports = Event; 
+
+            
+    
+    // Event.associate = (models) => {
+    //     models.Event.belongsTo(models.user, {
+    //         foreignKey: {
+    //             allowNull: false,
+    //     },
+    // });
+    //     models.Event.belongsTo(models.location, {
+    //         ForeignKey: "locationId",
+    //             });
+    //     };
+            // return Event; 
